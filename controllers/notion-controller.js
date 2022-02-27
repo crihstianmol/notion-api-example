f09076c913239ded3ee1b708789d0cd986d102d5
Crihstian Molina <crihstianmol@gmail.com>
2022-02-27 13:02:18 -0500

    refactor code and adding types

--- controllers/notion-controller.js
+++ controllers/notion-controller.js
@@ -26,54 +26,60 @@ const formatBlocks = async (blocks, blogs = []) => {
                         const blockc = await getBlocks(element.id);
                         blogs = await formatBlocks(blockc, blogs);
                         break;
-                    case "heading_2":
-                        category = element.heading_2.text[0].text.content;
-                        break;
                     case "child_page":
+                        const blogresponse = await getPages(element.id)
                         const myblogobject = {};
                         myblogobject.id = element.id;
                         myblogobject.category = category;
-                        const blogresponse = await getPages(element.id)
-                        if (blogresponse.icon.emoji) {
-                            myblogobject.emoji = blogresponse.icon.emoji;
-                        }
-                        if (blogresponse.properties.title) {
-                            if (blogresponse.properties.title.title[0].type === "text") {
-                                myblogobject.title = blogresponse.properties.title.title[0].text.content;
-                            }
-                        }
-                        if (blogresponse.cover.type === "external") {
-                            myblogobject.imgsrc = blogresponse.cover.external.url;
-                        }
+                        myblogobject.emoji = blogresponse.icon.emoji ? blogresponse.icon.emoji : undefined;
+                        myblogobject.title = blogresponse.properties.title ? (blogresponse.properties.title.title[0].type === "text" ? blogresponse.properties.title.title[0].text.content : undefined) : undefined;
+                        myblogobject.imgsrc = blogresponse.cover ? (blogresponse.cover.type === "external" ? blogresponse.cover.external.url : undefined) : undefined;
                         blogs.push(myblogobject)
                         break;
                     case "paragraph":
-                        let textsinparagraph = []
-                        textsinparagraph = element.paragraph.text.map(element => { return { annotations: element.annotations, text: element.plain_text } })
-                        // element.paragraph.text.forEach(element => {
-                        //     textsinparagraph.push({ annotations: element.annotations, text: element.plain_text, list: false, childs: [] });
-                        // });
-                        blogs.push({ paragraph: textsinparagraph });
+                        blogs.push({ paragraph: element.paragraph.text.map(element => ({ annotations: element.annotations, text: element.plain_text, hasurl: element.href !== null && element.href !== undefined ? true : false, url: element.href !== null ? element.href : undefined })) });
+                        break;
+                    case "heading_1":
+                        blogs.push({ header1: element.heading_1.text.map(element => ({ annotations: element.annotations, text: element.plain_text, hasurl: element.href !== null && element.href !== undefined ? true : false, url: element.href !== null ? element.href : undefined })) });
+                        break;
+                    case "heading_2":
+                        blogs.push({ header2: element.heading_2.text.map(element => ({ annotations: element.annotations, text: element.plain_text, hasurl: element.href !== null && element.href !== undefined ? true : false, url: element.href !== null ? element.href : undefined })) });
+                        break;
+                    case "heading_3":
+                        blogs.push({ header3: element.heading_3.text.map(element => ({ annotations: element.annotations, text: element.plain_text, hasurl: element.href !== null && element.href !== undefined ? true : false, url: element.href !== null ? element.href : undefined })) });
                         break;
                     case "bulleted_list_item":
                         let parentList = blogs[blogs.length - 1];
-                        parentList.paragraph[0].list = true
-                        parentList.paragraph[0].childs = element.bulleted_list_item.text.map(element => { return { annotations: element.annotations, text: element.plain_text } })
-                        // element.bulleted_list_item.text.forEach(element => {
-                        //     parentList.paragraph[0].childs.push({ annotations: element.annotations, text: element.plain_text });
-                        // });
+                        parentList.list = true
+                        parentList.childs = parentList.childs ? parentList.childs : [];
+                        parentList.childs = parentList.childs.concat(element.bulleted_list_item.text.map(element => ({ annotations: element.annotations, text: element.plain_text, hasurl: element.href !== null && element.href !== undefined ? true : false, url: element.href !== null ? element.href : undefined, ordered: false })))
                         blogs[blogs.length - 1] = parentList;
                         break;
                     case "numbered_list_item":
                         let numparentList = blogs[blogs.length - 1];
-                        numparentList.paragraph[0].list = true
-                        numparentList.paragraph[0].childs = element.numbered_list_item.text.map(element => { return { annotations: element.annotations, text: element.plain_text } })
-                        // element.numbered_list_item.text.forEach(element => {
-                        //     numparentList.paragraph[0].childs.push({ annotations: element.annotations, text: element.plain_text });
-                        // });
+                        numparentList.list = true
+                        numparentList.childs = numparentList.childs ? numparentList.childs : [];
+                        numparentList.childs = numparentList.childs.concat(element.numbered_list_item.text.map(element => ({ annotations: element.annotations, text: element.plain_text, hasurl: element.href !== null && element.href !== undefined ? true : false, url: element.href !== null ? element.href : undefined, ordered: true })))
                         blogs[blogs.length - 1] = numparentList;
                         break;
+                    case "image":
+                        blogs.push({ image: { caption: element.image.caption.map(element => (element.plain_text)).join(' '), url: element.image.external ? element.image.external.url : (element.image.file ? element.image.file.url : '') } })
+                        break
+                    case "video":
+                        blogs.push({ video: { caption: element.video.caption.map(element => (element.plain_text)).join(' '), url: element.video.external ? element.video.external.url : (element.video.file ? element.video.file.url : '') } })
+                        break
+                    case "embed":
+                        blogs.push({ embed: { caption: element.embed.caption.map(element => (element.plain_text)).join(' '), url: element.embed.url } });
+                        break
+                    case "quote":
+                        blogs.push({ quote: element.quote.text.map(element => ({ annotations: element.annotations, text: element.plain_text, hasurl: element.href !== null && element.href !== undefined ? true : false, url: element.href !== null ? element.href : undefined })) });
+                        break
+                    case "callout":
+                        blogs.push({ category: element.callout.text.map(element => ({ annotations: element.annotations, text: element.plain_text, hasurl: element.href !== null && element.href !== undefined ? true : false, url: element.href !== null ? element.href : undefined })) });
+                        break
                     default:
+                        // console.error("element not parsed")
+                        // console.info({ type: element.type, element: element })
                         break;
                 }
             }
